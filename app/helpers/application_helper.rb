e714644b18a8555e9c57c2db637206e865b26550
ruhenheim <asd.uchiyama@gmail.com>
2017-06-13 09:30:00 +0900

    bootstrap design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
   def status_select_list
-    [
-      ['未仕掛', '未仕掛'],
-      ['仕掛中', '仕掛中'],
-      ['完了', '完了'],
-    ]
+    %w(未仕掛 仕掛中 完了)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+include ApplicationHelper
+(1..100).each do |number|
+  Task.create(status: status_select_list[rand(3)], content: 'test content ' + number.to_s)
+end
