ef93bf780aa99884cb3f4ea5be0d1051c1037c58
ruhenheim <asd.uchiyama@gmail.com>
2017-06-14 08:53:55 +0900

    ユーザ登録機能

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def index
+    @users = User.all.page(params[:page])
+  end
+  def show
+  end
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'ユーザを登録しました.'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました.'
+      render :new
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
